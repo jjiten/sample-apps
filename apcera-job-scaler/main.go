29fc9707726010a408ddab4b9b75967bc35e86b2
Carlos Lopez Garces <carlos.lopez.garces@apcera.com>
2017-03-29 21:10:27 -0600

    Build different rescalers according to config type

--- apcera-job-scaler/main.go
+++ apcera-job-scaler/main.go
@@ -7,7 +7,22 @@ import (
 	"os"
 	"strconv"
 	"time"
+
+	"github.com/apcera/sample-apps/apcera-job-scaler/rescalers"
 )
+
+const (
+	Thresholds = "THRESHOLDS"
+	Feedback   = "FEEDBACK"
+)
+
+func printScalingJobConfig(config *ScalingJobConfig) {
+	fmt.Println("Scaling Job Config set...")
+	fmt.Println("FQN: ", config.jobFQN)
+	fmt.Println("Scaling Frequency: ", config.scalingFrequency)
+	fmt.Println("Minimum Instance limit: ", config.minInstances)
+	fmt.Println("Maximum Instance limit: ", config.maxInstances)
+}
 
 // getScalingJobConfig constructs the configurations of the scaling job and
 // its associated scaling triggers from environment variables if set, or else
@@ -20,8 +35,8 @@ func getScalingJobConfig() ScalingJobConfig {
 		fmt.Println("TARGET_JOB not set in environment")
 		os.Exit(1)
 	}
-
 	sj.jobFQN = jobFQN
+
 	scalingFrequency := os.Getenv("SCALING_FREQ")
 	if scalingFrequency == "" {
 		fmt.Println("SCALING_FREQ not set. Defaulting scaling frequency to 1 minute")
@@ -31,28 +46,14 @@ func getScalingJobConfig() ScalingJobConfig {
 		sj.scalingFrequency = time.Duration(sf) * time.Second
 	}
 
-	cpuRoof := os.Getenv("CPU_ROOF")
-	if cpuRoof == "" {
-		fmt.Println("CPU_ROOF not set. Defaulting cpu roof to 80%")
-		sj.cpuRoof = 80
-	} else {
-		sj.cpuRoof, _ = strconv.Atoi(cpuRoof)
+	rescalerType := os.Getenv("RESCALER")
+	if rescalerType == "" {
+		fmt.Println("RESCALER not set. Defaulting rescaler to THRESHOLDS")
+		rescalerType = Thresholds
 	}
-
-	cpuFloor := os.Getenv("CPU_FLOOR")
-	if cpuFloor == "" {
-		fmt.Println("CPU_FLOOR not set. Defaulting cpu floor to 20%")
-		sj.cpuFloor = 20
-	} else {
-		sj.cpuFloor, _ = strconv.Atoi(cpuFloor)
-	}
-
-	instanceCounter := os.Getenv("INSTANCE_COUNTER")
-	if instanceCounter == "" {
-		fmt.Println("INSTANCE_COUNTER not set. Defaulting instance counter to 1")
-		sj.instanceCounter = 1
-	} else {
-		sj.instanceCounter, _ = strconv.Atoi(instanceCounter)
+	if rescalerType != Thresholds || rescalerType != Feedback {
+		fmt.Println("RESCALER unknown. Defaulting rescaler to THRESHOLDS")
+		rescalerType = Thresholds
 	}
 
 	maxInstances := os.Getenv("MAX_INSTANCES")
@@ -71,22 +72,116 @@ func getScalingJobConfig() ScalingJobConfig {
 		sj.minInstances, _ = strconv.Atoi(minInstances)
 	}
 
-	fmt.Println("Scaling Job Config set...")
-	fmt.Println("FQN: ", sj.jobFQN)
-	fmt.Println("Scaling Frequency: ", sj.scalingFrequency)
-	fmt.Println("Lower CPU limit: ", sj.cpuFloor)
-	fmt.Println("Upper CPU limit: ", sj.cpuRoof)
-	fmt.Println("Minimum Instance limit: ", sj.minInstances)
-	fmt.Println("Maximum Instance limit: ", sj.maxInstances)
-	fmt.Println("No. of Instances to be added/removed when scaling behavior triggered: ", sj.instanceCounter)
+	rescaler, err := makeRescaler(rescalerType)
+	if err != nil {
+		panic(err)
+	}
+	sj.rescaler = rescaler
+
 	return sj
+}
+
+func getThresholdsRescalerConfig() *rescalers.ThresholdsRescalerConfig {
+	config := &rescalers.ThresholdsRescalerConfig{}
+
+	cpuRoof := os.Getenv("CPU_ROOF")
+	if cpuRoof == "" {
+		fmt.Println("CPU_ROOF not set. Defaulting cpu roof to 80%")
+		config.Upper = 80.0
+	} else {
+		config.Upper, _ = strconv.ParseFloat(cpuRoof, 64)
+	}
+
+	cpuFloor := os.Getenv("CPU_FLOOR")
+	if cpuFloor == "" {
+		fmt.Println("CPU_FLOOR not set. Defaulting cpu floor to 20%")
+		config.Lower = 20.0
+	} else {
+		config.Lower, _ = strconv.ParseFloat(cpuFloor, 64)
+	}
+
+	instanceCounter := os.Getenv("INSTANCE_COUNTER")
+	if instanceCounter == "" {
+		fmt.Println("INSTANCE_COUNTER not set. Defaulting instance counter to 1")
+		config.Size = 1
+	} else {
+		config.Size, _ = strconv.Atoi(instanceCounter)
+	}
+
+	inverted := os.Getenv("INVERTED")
+	if inverted == "" {
+		fmt.Println("INVERTED not set. Defaulting to false")
+		config.Inverted = false
+	} else {
+		config.Inverted = true
+	}
+
+	return config
+}
+
+func getFeedbackRescalerConfig() *rescalers.FeedbackRescalerConfig {
+	config := &rescalers.FeedbackRescalerConfig{}
+
+	setpoint := os.Getenv("SETPOINT")
+	if setpoint == "" {
+		fmt.Println("SETPOINT not set. Defaulting setpoint to 50%")
+		config.Setpoint = 50.0
+	} else {
+		config.Setpoint, _ = strconv.ParseFloat(setpoint, 64)
+	}
+
+	kp := os.Getenv("KP")
+	if kp == "" {
+		fmt.Println("KP not set. Defaulting KP to 4")
+		config.KP = 4.0
+	} else {
+		config.KP, _ = strconv.ParseFloat(kp, 64)
+	}
+
+	ki := os.Getenv("KI")
+	if ki == "" {
+		fmt.Println("KI not set. Defaulting KI to 0")
+		config.KI = 0.0
+	} else {
+		config.KI, _ = strconv.ParseFloat(ki, 64)
+	}
+
+	kd := os.Getenv("KD")
+	if kd == "" {
+		fmt.Println("KD not set. Defaulting KD to 0")
+		config.KD = 0.0
+	} else {
+		config.KD, _ = strconv.ParseFloat(kd, 64)
+	}
+
+	inverted := os.Getenv("INVERTED")
+	if inverted == "" {
+		fmt.Println("INVERTED not set. Defaulting to false")
+		config.Inverted = false
+	} else {
+		config.Inverted = true
+	}
+
+	return config
+}
+
+func makeRescaler(rescalerType string) (rescalers.Rescaler, error) {
+	if rescalerType == Thresholds {
+		return rescalers.NewThresholdsRescaler(getThresholdsRescalerConfig()), nil
+	}
+	if rescalerType == Feedback {
+		return rescalers.NewFeedbackRescaler(getFeedbackRescalerConfig()), nil
+	}
+	return nil, fmt.Errorf("Unknown rescaler type %s", rescalerType)
 }
 
 func main() {
 	// The DefaultJobScaler / the default scaling algorithm to be used
 	// for making scaling decisions.
+	config := getScalingJobConfig()
+	printScalingJobConfig(&config)
 	jobScaler := NewJobScaler()
-	jobScaler.EnableAutoScale(getScalingJobConfig())
+	jobScaler.EnableAutoScale(config)
 
 	for {
 		select {
